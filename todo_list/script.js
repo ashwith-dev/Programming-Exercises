9f04cd4b5056c161532431a5e400c5b3a9888baa
ashwith-dev <ashwiththatipally@gmail.com>
2026-01-27 17:00:01 +0530

    todo list js

---
+++ todo_list/script.js
@@ -0,0 +1,29 @@
+const input = document.getElementById("input");
+const btn = document.getElementById("addBtn");
+const list = document.getElementById("list");
+
+let todos = [];
+
+function addTodo() {
+    let inputText = input.value;
+
+    if (inputText == "") {
+        alert("Please enter a todo");
+        return;
+    }
+
+    todos.push(inputText);
+    input.value = "";
+
+    show();
+}
+
+function show() {
+    list.innerHTML = "";
+
+    for (let i = 0; i < todos.length; i++) {
+        list.innerHTML += (i + 1) + ". " + todos[i]+ "<br>";
+    }
+}
+
+
